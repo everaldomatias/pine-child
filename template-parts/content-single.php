5196ded8d18182aa729e8d3d96c48fb7238916b7
everaldomatias <matiaseveraldo@gmail.com>
2017-11-25 21:13:20 -0200

    Iniciando funcionalidade de formulario na single Perfil.

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -21,6 +21,8 @@
 			$facebook	= get_post_meta( $post_id, 'facebook', true );
 			$site		= get_post_meta( $post_id, 'site', true );
 			$link 		= get_post_meta( $post_id, 'link', true );
+			$to_report	= get_post_meta( $post_id, 'to_report', true );
+
 		?>
 
 		<?php if ( ! empty( $pes ) ): ?>
@@ -43,8 +45,16 @@
 			<a class="link" href="<?php echo esc_url( $link ); ?>" target="_blank">Outro link</a>
 		<?php endif; ?>
 
+		<?php if ( ! empty( $to_report ) ): ?>
+			<div class="report">
+				<span><?php _e( 'Use o formulário abaixo para reportar informações erradas, abuso ou ofensas nesse perfil. Sua identidade será preservada e seus dados utilizados apenas para entrar em contato caso necessário.', 'pine_child' ); ?></span>
+				<?php echo do_shortcode( '[contact-form-7 title="Denunciar"]' ); ?>
+				<span><?php _e( 'Desde já muito obrigado por ajudar para que nosso acervo digital tenha cada vez mais qualidade.', 'pine_child' ); ?></span>
+			</div><!-- report -->
+		<?php endif; ?>
+
 		<?php wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'pine' ),
+			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'pine_child' ),
 			'after'  => '</div>',
 		) ); ?>
 	</article><!-- #post-## -->
